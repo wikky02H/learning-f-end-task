e4cf588b1cb2e1fb41c28deff84eb036615f396f
wikkyO2H <vignesh@vhypotenuse.com>
2023-04-20 18:13:42 +0530

    task_4

--- task.js
+++ task.js
@@ -1,54 +1,38 @@
-for(let i=1; i<11; i++){
-    console.log("value",i);
+let value = 0;
+while(true){
+    value +=1
+    if(value > 10){
+    break;
+    }
+console.log("value",value)
 }
 
-let number_1 = 10;
-let number_2 = 20;
-let numbers = number_1 + number_2;
-console.log("num",numbers);
-while(numbers <= 50){
-    number_1 ++;
-    number_2 += 2;
-    numbers = number_1 + number_2;
-    console.log("num",numbers);
-}
+let city = [
+                "Bangalore",
+                "Kolkata",
+                "chennai",
+                "Ahemdabad",
+                "Hyderabad",
+                "Pune",
+                "Delhi",
+                "Mumbai",
+                "Madurai",
+                "Indore",
+                "eeeeee"
+           ];
 
-const person =
-{
-    label: "Box",
-    dimension: "22x33x22"
-}
+const push = city.push("sivakasi");
+console.log("city",push);
 
-// const box = {
-//     label: "Box",
-//     dimension: "22x33x22"
-//   };
+const unshift = city.unshift("coimbatore");
+console.log("kolkata",unshift);
 
-//   for (let key in box) {
-//     console.log("key:" + key + "  value:" + box[key]);
-//   }
+const slice = city.slice(0, 5);
+console.log("slice",slice)
 
-const box = {
-    label: "Box",
-    dimension: "22x33x22"
-  };
-//   let key = "";
-  for (let key in box) {
-    
-    console.log("key:" + key +" value:"+box[key])
-    // console.log(key + ": " + box[key]);
-  }
-  
-  const number = [10,20,30];
-
-  for(let key in number){
-    console.log("number", "index :"+key+" value:"+number[key])
-  }
+const des = city.sort((a,b) => b.localeCompare(a))
+console.log("sort",des)
 
 
-const num = [10,20,30,40,50];
-
-for(let [index, value] of num.entries()){
-    console.log("index: "+(index+" value:"+value));
-}
-
+const li1 = city.filter(city => city.length < 5)
+console.log("check",li1)
