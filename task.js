7cf77de5ac81f0a5e74a15263fe5dc306e858d9a
wikkyO2H <vignesh@vhypotenuse.com>
2023-04-24 17:03:52 +0530

    task_5

--- task.js
+++ task.js
@@ -1,38 +1,70 @@
-let value = 0;
-while(true){
-    value +=1
-    if(value > 10){
-    break;
+// let value = 0;
+// while(true){
+//     value +=1
+//     if(value > 10){
+//     break;
+//     }
+// console.log("value",value)
+// }
+
+// let city = [
+//                 "Bangalore",
+//                 "Kolkata",
+//                 "chennai",
+//                 "Ahemdabad",
+//                 "Hyderabad",
+//                 "Pune",
+//                 "Delhi",
+//                 "Mumbai",
+//                 "Madurai",
+//                 "Indore",
+//                 "eeeeee"
+//            ];
+
+// const push = city.push("sivakasi");
+// console.log("city",push);
+
+// const unshift = city.unshift("coimbatore");
+// console.log("kolkata",unshift);
+
+// const slice = city.slice(0, 5);
+// console.log("slice",slice)
+
+// const des = city.sort((a,b) => b.localeCompare(a))
+// console.log("sort",des)
+
+
+// const li1 = city.filter(city => city.length < 5)
+// console.log("check",li1)
+
+class Car {
+    constructor(name, model) {
+      this.name = name;
+      this.model = model;
     }
-console.log("value",value)
-}
+  
+    getModel() {
+      return this.model;
+    }
+  
+    setModel(model) {
+      this.model = model;
+    }
+    getName(){
+        return this.name;
+    }
+    setName(name){
+        this.name = name;
+    }
+  }
 
-let city = [
-                "Bangalore",
-                "Kolkata",
-                "chennai",
-                "Ahemdabad",
-                "Hyderabad",
-                "Pune",
-                "Delhi",
-                "Mumbai",
-                "Madurai",
-                "Indore",
-                "eeeeee"
-           ];
-
-const push = city.push("sivakasi");
-console.log("city",push);
-
-const unshift = city.unshift("coimbatore");
-console.log("kolkata",unshift);
-
-const slice = city.slice(0, 5);
-console.log("slice",slice)
-
-const des = city.sort((a,b) => b.localeCompare(a))
-console.log("sort",des)
-
-
-const li1 = city.filter(city => city.length < 5)
-console.log("check",li1)
+  const myCar = new Car("Toyota", "Corolla");
+  console.log(myCar.getModel()); 
+//   const myCar = new Car()
+  console.log(myCar.getName()); 
+  
+  myCar.setModel("Camry","Bonbon");
+  console.log(myCar.getModel()); 
+  
+//   myCar.setName("Bonbon");
+  console.log(myCar.getName())
